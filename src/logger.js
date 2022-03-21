00c79ae5f6a807f59d4cc98f2c28a230340e5d71
Vlad <vvllaadd1233345@gmail.com>
2022-03-21 15:50:54 +0200

    feat: update logger

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,44 @@
 /* eslint-disable no-undef */
 const fs = require("fs");
 const path = require("path");
+const {performance} = require('perf_hooks');
 
-const pathLog = path.join(__dirname, "log.txt");
+const date = new Date();
+
+const fileName = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-log`
+
+const pathLogDir = path.join(process.env.HOME, "EventsExpressNode-logs");
+const pathLogFile = path.join(pathLogDir, fileName);
+
+const isAccessible = path => {
+  return fs
+    .accessSync(path)
+};
 
 const logger = (req, _, next) => {
   try {
     const start = performance.now();
     next();
 
+    try {
+      isAccessible(pathLogDir)
+    } catch (error) {
+      fs.mkdirSync(pathLogDir);
+    }   
+
+    try {
+      isAccessible(pathLogFile)
+    } catch (error) {
+      const perf = performance.now() - start;
+      const newLog = `${req.method} ${req.originalUrl} ${perf}`;
+      fs.writeFileSync(pathLogFile, newLog);
+    
+    return
+    }
+
     const perf = performance.now() - start;
-    const newFile = `\n${req.method} ${req.originalUrl} ${perf}`;
-    fs.appendFileSync(pathLog, newFile);
+    const newLog = `\n${req.method} ${req.originalUrl} ${perf}`;
+    fs.appendFileSync(pathLogFile, newLog);
   } catch (error) {
     console.log(error);
   }
