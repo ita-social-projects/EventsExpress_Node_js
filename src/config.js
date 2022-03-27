5c61ded2918f1cc69bc9a69b5d6bd917090360f1
Vlad <vvllaadd1233345@gmail.com>
2022-03-27 23:22:04 +0300

    feat: docker image

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, PORT = 3000, DB_HOST } = process.env;
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  PORT = 3000,
+  DB_HOST,
+  DB_PORT,
+} = process.env;
 
-module.exports = { DB_NAME, DB_USER, DB_PASSWORD, PORT, DB_HOST };
+module.exports = { DB_NAME, DB_USER, DB_PASSWORD, PORT, DB_HOST, DB_PORT };
--- src/databaseSetup.js
+++ src/databaseSetup.js
@@ -1,8 +1,16 @@
 const Sequelize = require("sequelize");
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, PORT } = require("./config");
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  PORT,
+} = require("./config");
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: "mysql",
   /* FOR MACOS */
   dialectOptions: {
