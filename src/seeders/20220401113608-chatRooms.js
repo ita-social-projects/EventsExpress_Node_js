15313402daddd98c0750e700a31a1fec4d729e86
Stanislav-Vyhnan <stas.vignan@gmail.com>
2022-04-03 19:59:08 +0300

    feat: created seed, a new script, fixed some problems with seeds

---
+++ src/seeders/20220401113608-chatRooms.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "chatrooms",
+      [
+        {
+          title: "Chat room 1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Chat room 2",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("chatrooms", null, {});
+  },
+};
